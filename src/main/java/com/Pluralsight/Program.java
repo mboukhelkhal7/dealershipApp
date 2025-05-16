5acef31e348e17592817dfbd5fe93a7b08e4e1f3
Mahmoud <maboukhelkhal@gmail.com>
2025-05-16 13:03:58 -0700

    create a menu

--- src/main/java/com/Pluralsight/Program.java
+++ src/main/java/com/Pluralsight/Program.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        DealershipFileManager fileManager = new DealershipFileManager();
-        Dealership dealership = fileManager.getDealership();
 
-        System.out.println("Dealership: " + dealership.getName());
-        for (Vehicle vehicle : dealership.getAllVehicles()) {
-            System.out.println(vehicle);
-        }
     }
 }
--- src/main/java/com/Pluralsight/UserInterface.java
+++ src/main/java/com/Pluralsight/UserInterface.java
@@ -1,4 +1,62 @@
 package com.Pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+
+    private Dealership dealership;
+
+    public void display() {
+
+        init();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n=== Car Dealership Menu ===");
+            System.out.println("1. View All Vehicles");
+            System.out.println("99. Quit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    // Show all vehicles directly
+                    List<Vehicle> vehicles = dealership.getAllVehicles();
+
+                    if (vehicles.isEmpty()) {
+                        System.out.println("No vehicles found in inventory.");
+                    } else {
+                        System.out.println("\n=== Vehicle Inventory ===");
+                        for (Vehicle v : vehicles) {
+                            System.out.println(v);
+                        }
+                    }
+                    break;
+
+                case 99:
+                    System.out.println("Exiting the program. Goodbye!");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+
+            }
+        }
+        scanner.close();
+    }
+    private void init() {
+        DealershipFileManager manager = new DealershipFileManager();
+        dealership = manager.getDealership();
+    }
+
+
 }
+
+
+
